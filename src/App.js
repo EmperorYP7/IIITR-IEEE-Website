80c3562b7b8acf3655f8dfa9dba87ccb9d16506a
Yash Pandey <62606998+EmperorYP7@users.noreply.github.com>
2020-07-28 16:57:54 +0530

    Minor update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ function App() {
   return (
     <div className="App">
       <h1>Indian Institute of Information Technology, Ranchi IEEE Student Branch Website.</h1>
+      <p>
+      A natural way to approach the problem is to introduce some sort of clock that is
+coupled to the particle, and to define the tunneling time in terms of the change in the
+clock variable from the time that the particle reaches the barrier to the time it emerges. It
+is then possible to define the expectation time for the tunneling event in terms of the
+expectation value of the clock variable, which is a quantum observable. Several
+suggestions for quantum clocks have appeared in the literature [5]. In this paper I restrict
+attention to an early proposal for a quantum clock by Salecker and Wigner [6], and later
+elaborated by Peres [7]. This model deals not with moving wave packets or other timedependent states, but with stationary states for which the time enters as a changing phase:
+e
+iEt. The quantum clock essentially measures the change in phase, resulting in a duration
+known as the phase time. Because one is dealing with stationary states, the clock
+measures only time differences between two events, not the absolute time of either event. 
+      </p>
     </div>
   );
 }
