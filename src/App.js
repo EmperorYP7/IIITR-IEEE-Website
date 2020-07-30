da0991c48c1c834357c78c809549c43acc881ab8
EmperorYP7 <pandeyyash04@gmail.com>
2020-07-31 00:43:27 +0530

    added dummy files

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ expectation value of the clock variable, which is a quantum observable. Several
 suggestions for quantum clocks have appeared in the literature [5]. In this paper I restrict
 attention to an early proposal for a quantum clock by Salecker and Wigner [6], and later
 elaborated by Peres [7]. This model deals not with moving wave packets or other timedependent states, but with stationary states for which the time enters as a changing phase:
-e
-iEt. The quantum clock essentially measures the change in phase, resulting in a duration
+eiEt. The quantum clock essentially measures the change in phase, resulting in a duration
 known as the phase time. Because one is dealing with stationary states, the clock
 measures only time differences between two events, not the absolute time of either event. 
       </p>
