caed7a1c496b306e5666b80062721a6a42d39210
EmperorYP7 <pandeyyash04@gmail.com>
2020-07-31 14:07:35 +0530

    integrating react and express

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import About from './components/about';
+
+function App() {
+  return (
+    <div className="App">
+      <About />
+    </div>
+  );
+}
+
+export default App;
