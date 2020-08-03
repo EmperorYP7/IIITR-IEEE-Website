c4d214c79eabc89b00aa99662ee543a348ffbc00
tusharjain0022 <tusharjain0022@gmail.com>
2020-08-04 01:52:16 +0530

    footer updated

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -8,40 +8,53 @@ class Footer extends Component {
     render() {
         return (
             <div>
-                  <footer className="footer">
+    <footer className="footer pb-0">
         <div className="container">
-            <div className="row">
-                <div className="col-12 col-sm-4 ">
-
-                    <div id="connect" className="text-center ">
+            <div className="row"> 
+                <div className="col-12 col-md-6 ">
+                    <div className="text-center ">
                         <h5>Connect with us</h5><hr></hr>
-                        <a className=" btn text-white" href="http://"><i className="fab fa-facebook-square fa-2x"></i></a>
-                        <a className=" btn text-white" href="http://"><i className="fab fa-twitter-square fa-2x"></i></a>
-                        <a className=" btn text-white" href="http://"><i className="fab fa-linkedin fa-2x"></i></a>
-                        <a className=" btn text-white" href="http://"><i className="fab fa-instagram fa-2x"></i></a>
+                        <a className=" btn text-white" href="#"><i className="fab fa-facebook-square fa-2x"></i></a>
+                        <a className=" btn text-white" href="#"><i className="fab fa-twitter-square fa-2x"></i></a>
+                        <a className=" btn text-white" href="#"><i className="fab fa-linkedin fa-2x"></i></a>
+                        <a className=" btn text-white" href="#"><i className="fab fa-instagram fa-2x"></i></a>
                         <a className=" btn text-white" href="mailto:iiitranchi@contact.info"><i className="fas fa-envelope fa-2x"></i></a>
                     </div>
-                </div>
-
-                <div className="col-7 col-sm-3 offset-3 text-center ">
+                </div>            
+                <div className="col-12 col-md-6 text-center addr">
                     <h5>Our Address</h5><hr></hr>
                     <address>
-                        Sirkha Toli, Kali Nagar, Namkum,
-                        Ranchi, Jharkhand 834010
-                        Tel : 123-24578
-                        Mail : <a href="mailto:iiitranchi@contact.info">iiitranchi@contact.info</a>
+                        <ul className="list-unstyled">
+                            <li>Sirkha Toli, Kali Nagar, Namkum, </li>
+                            <li>Ranchi, Jharkhand 834010</li>
+                            <li>Tel : 123-24578</li>
+                            <li>Mail : <a href="mailto:iiitranchi@contact.info">iiitranchi@contact.info</a></li>
+                        </ul>
+                        
+                        
+                        
                         <br></br>
-		              </address>
+		            </address>
                 </div>
-
-           </div>
-           <div className="row justify-content-center mt-5">
-               <hr></hr>
-                <div className="col-auto ">
-                    <p>Web Team IIITRanchi © Copyright 2020 </p>
-                </div>
-           </div>
+            </div>
         </div>
+        <div className="container-fluid">
+            <div className="row justify-content-center mt-5 pt-3"> 
+                <div  className="col-1 ">
+                    <a id="dev" className=" btn text-white " href="#"><i className="fas fa-laptop-code fa-2x"></i><span>Developers</span></a>
+                 </div>   
+                    
+                 <div className="col-10 text-center">
+                     <hr></hr>
+                     <p>Web Team IIITRanchi © Copyright 2020 </p>
+                 </div>
+                 <div className="col-1">
+                    <a id="up" className=" btn text-white " href="#"><i className="fas fa-chevron-circle-up fa-2x"></i><br></br><span>Back to Top</span></a>
+                </div>
+            </div>
+           </div>
+           
+        
     </footer>
             </div>
         );
