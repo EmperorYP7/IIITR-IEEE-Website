e5b8d715f4f843e4bed454f205867873cefda92e
tusharjain0022 <tusharjain0022@gmail.com>
2020-08-05 19:20:02 +0530

    images fixed in society card

--- frontend/src/components/Home/Society/societycards.js
+++ frontend/src/components/Home/Society/societycards.js
@@ -3,23 +3,23 @@ export const CARDS =
         {
         id: 0,
         name:'CSE',
-        image: './images/vaporeon.png',
+        image: 'assets/images/vaporeon.png',
         shortdescription:'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        fulldescription:' #1111 : when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        fulldescription:' #1111 : B. Tech CSE is an engineering course which is based in various engineering disciplines such as computer engineering, mechanical engineering, communications, electronic, Software engineering etc. along with the knowledge of computer systems. specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
        },
        {
         id: 1,
         name:'ECE',
-        image: './images/eeve.jpg',
+        image: 'assets/images/eeve.jpg',
         shortdescription:'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
         fulldescription:'#2222 :  when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
        },
        {
         id: 2,
         name:'ML',
-        image: './images/pikachu.png',
+        image: 'assets/images/pikachu.png',
         shortdescription:'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        fulldescription:' #3333 : when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        fulldescription:' #3333 :Machine learning is the study of computer algorithms that improve automatically through experience. It is seen as a subset of artificial intelligenceplines such as computer engineering, mechanical engineering, communications, electronic, Software engineering etc. along with the knowledge of computer systems.it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
        },
 
     ];
