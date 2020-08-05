d1a78533b191ce188e63b89b4c83d8f7808743c5
hariom1625 <hariomyadav.btech.cs19@iiitranchi.ac.in>
2020-08-05 17:11:23 +0530

    Landing page fixed

--- frontend/src/components/Home/LandingPage/LandingPage.js
+++ frontend/src/components/Home/LandingPage/LandingPage.js
@@ -6,18 +6,17 @@ class LandingPage extends Component {
 
     render() {
         return (
-            <div className="container-fluid bg-nmg">
-                <div className="row">
-                    <div className="col-sm-12 col-md-12 col-lg-5 text-sm-center text-md-center ">
-                        <img className="image-lander img-fluid" src={landercard} alt="Logo"></img>
+            <div >
+                <div className="row landercard">
+                    <div className="col-lg-6 col-md-12  ">
+                        <img  src={landercard} alt="Logo"></img>
                     </div>
-                    <div className="col-lg-1"></div>
-                    <div className="col-sm-12 col-md-12 col-lg-5 float-lg-right">
-                        <p className="about-content"><span className="about-head"><span className="head-quote">"</span>Branch Name </span>is the IEEE SB iiit ranchi. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                    <div className="col-lg-6 col-md-12">
+                        <p className="about-content"><span className="about-head">"Branch Name </span>is the IEEE SB iiit ranchi. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
                     </div>
                 </div>
             </div>
-            
+
 
 
         );
