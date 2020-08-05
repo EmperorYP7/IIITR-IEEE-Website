215b73a7052a1f0cc8223491e1b0e0841733b143
EmperorYP7 <pandeyyash04@gmail.com>
2020-08-05 17:38:47 +0530

    minor updates

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -5,8 +5,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     const [active, setActive] = useState('Home');
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-nmg">
-                <a className="navbar-brand px-5" href="/">LOGO</a>
+            <nav className="navbar navbar-expand-lg navbar-light bg-nmg py-3">
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
