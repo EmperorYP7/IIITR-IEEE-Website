3b39cfbee783ca7ae7274b5b6703ef859431f620
hariom1625 <hariomyadav.btech.cs19@iiitranchi.ac.in>
2020-08-07 02:43:51 +0530

    added hover

--- frontend/src/components/Home/Society/Society.js
+++ frontend/src/components/Home/Society/Society.js
@@ -7,14 +7,18 @@ import CardColumns from 'react-bootstrap/CardColumns';
 import Button from 'react-bootstrap/Button';
 import {CARDS} from './societycards';
 
+
 class Society extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+
        selectedSociety: null,
        cards : CARDS
     }
 }
+
+
 societySelect(id) {
     this.setState({ selectedSociety: id});
 }
@@ -23,11 +27,12 @@ societySelect(id) {
     if(card !=null){
       return (
         <div className="box mb-5">
-        <div className="col-12">
-           <h3>Student Chapter </h3>
+        <div className="col-lg-12 col-md-12">
+           <h3>{card.name} </h3>
           <p>
+
             {card.fulldescription}
-           
+
          </p>
         </div>
         </div>
@@ -35,17 +40,17 @@ societySelect(id) {
     }
     else {
       return(
-        <div></div>
+<div> </div>
       );
     }
    }
-   
+
     render() {
-      
+
 
         const show = this.state.cards.map((card) => {
           return (
-              
+
                <Card>
                 <Card.Img variant="top" className="img" src={card.image} />
                 <Card.Body>
@@ -56,22 +61,23 @@ societySelect(id) {
                 </Card.Body>
                 <Card.Footer>
         <small className="text-muted">
-          <Button variant="dark"  onClick={() => this.societySelect(card)} className="button">Read More</Button></small>
+          <Button   variant="dark" onMouseEnter={() => this.societySelect(card)}
+          onMouseLeave={() =>this.societySelect(null)} className="button">Read More</Button>
+      </small>
                 </Card.Footer>
               </Card>
-            
+
           );
       });
         return (
-            <div className="container-fluid bg-nmg mx-0 my-0">
+
+            <div className="container-fluid bg-nmg mx-0 my-0" >
           <CardColumns className="deck">
 
               {show}
 </CardColumns>
 
-{this.rendersociety(this.state.selectedSociety)}
-
-
+        {this.rendersociety(this.state.selectedSociety)}
             </div>
         );
     }
