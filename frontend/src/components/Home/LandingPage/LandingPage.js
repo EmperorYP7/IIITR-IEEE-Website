6b3d3806d0517c35d8be9e877ca8080c27e07387
RishavMz <58086511+RishavMz@users.noreply.github.com>
2020-08-07 10:27:39 +0530

    Parallax scrolling in home

--- frontend/src/components/Home/LandingPage/LandingPage.js
+++ frontend/src/components/Home/LandingPage/LandingPage.js
@@ -7,9 +7,9 @@ class LandingPage extends Component {
     render() {
         return (
             <div >
-                <div className="row landercard leaves my-0">
+                <div className="row landercard leaves my-0" id="parallax">
                     <div className="col-lg-6 col-md-12">
-                        <img  src={landercard} className="img-fluid" alt="Logo"></img>
+                        <img  src={landercard} className="img-fluid"  alt="Logo"></img>
                     </div>
                     <div className="col-lg-6 col-md-12">
                         <p className="about-content"><span className="about-head">"Branch Name </span>is the IEEE SB iiit ranchi. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
