a048114c6a0813b10b3d74b6eefdb691807da6a6
hariom1625 <hariomyadav.btech.cs19@iiitranchi.ac.in>
2020-08-07 16:34:20 +0530

    added animation

--- frontend/src/components/Main/Home/LandingPage/LandingPage.js
+++ frontend/src/components/Main/Home/LandingPage/LandingPage.js
@@ -2,20 +2,31 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './LandingPage.css';
 import landercard from './landercard.svg';
+import iiitlogo from './IIITlogo.svg';
+
 class LandingPage extends Component {
 
     render() {
         return (
-            <div >
-                <div className="row landercard leaves my-0" id="parallax">
-                    <div className="col-lg-6 col-md-12">
-                        <img  src={landercard} className="img-fluid"  alt="Logo"></img>
-                    </div>
-                    <div className="col-lg-6 col-md-12">
-                        <p className="about-content"><span className="about-head">"Branch Name </span>is the IEEE SB iiit ranchi. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-                    </div>
-                </div>
-            </div>
+          <div className="row landercard leaves my-0" id="parallax">
+          <div class="circle-container">
+          	<div class="outer-ring"></div>
+
+          	<div class="outer-outer-ring"></div>
+
+          	<div class="circle">
+          		<div class="front">
+              <img  src={landercard} className="img-fluid"  alt="Logo"></img>
+
+          		</div>
+          		<div class="back">
+              <img  src={iiitlogo} className="img-fluid"  alt="Logo"></img>
+
+          		</div>
+          	</div>
+          </div>
+          </div>
+
 
 
 
