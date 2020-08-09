b6d81f0e0c334178aefb4f266c19ba5514fbeb9a
RishavMz <58086511+RishavMz@users.noreply.github.com>
2020-08-09 16:50:09 +0530

    Login Authentication

--- frontend/src/components/Admin/Admin.js
+++ frontend/src/components/Admin/Admin.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Events from './Events/Events';
 import Members from './Members/Members';
+import Login from './login/login';
 import './Admin.css';
 
 class Admin extends Component {
@@ -9,9 +10,10 @@ class Admin extends Component {
     render() {
         return (
             <div className="container bg-nmg">
-                <Events/>
-                <hr/>
-                <Members/>
+                
+                <Login/>
+        {/*console.log(this.props)*/}
+                
             </div>
         );
     }
