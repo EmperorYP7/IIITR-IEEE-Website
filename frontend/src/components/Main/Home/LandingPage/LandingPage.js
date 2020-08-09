35b4ef3a70082de08ca1c22680a76756701607bd
tusharjain0022 <tusharjain0022@gmail.com>
2020-08-09 14:58:43 +0530

    lander page animation fixed

--- frontend/src/components/Main/Home/LandingPage/LandingPage.js
+++ frontend/src/components/Main/Home/LandingPage/LandingPage.js
@@ -9,22 +9,30 @@ class LandingPage extends Component {
     render() {
         return (
           <div className="row landercard leaves my-0" id="parallax">
+          <div className="col-12 col-lg-5">
           <div class="circle-container">
           	<div class="outer-ring"></div>
 
           	<div class="outer-outer-ring"></div>
 
           	<div class="circle">
-          		<div class="front">
-              <img  src={landercard} className="img-fluid"  alt="Logo"></img>
+          		<div class="front ">
+              <img  src={landercard} className="img-fluid front-logo"  alt="Logo"></img>
 
           		</div>
-          		<div class="back">
-              <img  src={iiitlogo} className="img-fluid"  alt="Logo"></img>
+          		<div class="back ">
+              <img  src={iiitlogo} className="img-fluid back-logo"  alt="Logo"></img>
 
           		</div>
           	</div>
           </div>
+          </div>
+          {/* <div className="col-12 col-lg-7">
+            <div className="box text-white">
+              TITLE
+            </div>
+
+          </div> */}
           </div>
 
 
