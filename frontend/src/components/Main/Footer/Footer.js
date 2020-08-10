b4354dc4d67ff3c32e32ebac685e423d01119600
tusharjain0022 <tusharjain0022@gmail.com>
2020-08-10 15:21:50 +0530

    developer modal added in footer

--- frontend/src/components/Main/Footer/Footer.js
+++ frontend/src/components/Main/Footer/Footer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Footer.css';
 import { Link } from 'react-router-dom';
+import tushar from "./tushar.png";
 
 
 class Footer extends Component {
@@ -9,6 +10,125 @@ class Footer extends Component {
     render() {
         return (
             <div>
+                {/* <!-- Modal --> */}
+<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog modal-xl">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="col-12 modal-title text-center" id="exampleModalLabel">DEVELOPERS</h5>
+        <button type="button" className="close text-center" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true"><i class="fas fa-times fa-2x"></i></span>
+        </button>
+      </div>
+      <div className="modal-body">
+        <div className="container">
+        <div class="row row-content justify-content-center"> 
+            <div className="col-12 text-center"><h2>Full Stack</h2><hr></hr></div> 
+            <div class="col-12 col-lg-6">
+                <div class="media">
+                    <img class="d-flex mr-3 img-thumbnail align-self-center small-image"
+                            src={tushar} alt="YASH PANDEY"></img>
+                    <div class="media-body">
+                        <h2 class="mt-2">YASH PANDEY
+                        </h2>
+                        <p class="d-none d-sm-block">Musician/ Coder/ Blogger/ Youtube/ Developer/ Influencer/ Football /TT / Dancer/ Playboy/ Mumbaikar</p>
+                        <br></br>
+                        <a className=" btn text-dark" href="https://"><i className="fab fa-linkedin fa-2x"></i></a>
+                        <a className=" btn text-dark" href="https://"><i className="fab fa-github fa-2x"></i></a>
+                        <a className=" btn text-dark" href="mailto:iiitranchi@contact.info"><i className="fas fa-envelope fa-2x"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6 my-">
+            <div class="media">
+                    <img class="d-flex mr-3 img-thumbnail align-self-center small-image"
+                            src={tushar} alt="YASH PANDEY"></img>
+                    <div class="media-body">
+                        <h2 class="mt-2">YASH PANDEY
+                        </h2>
+                        <p class="d-none d-sm-block">Musician/ Coder/ Blogger/ Youtube/ Developer/ Influencer/ Football /TT / Dancer/ Playboy/ Mumbaikar</p>
+                        <br></br>
+                        <a className=" btn text-dark" href="https://"><i className="fab fa-linkedin fa-2x"></i></a>
+                        <a className=" btn text-dark" href="https://"><i className="fab fa-github fa-2x"></i></a>
+                        <a className=" btn text-dark" href="mailto:iiitranchi@contact.info"><i className="fas fa-envelope fa-2x"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row row-content justify-content-center"> 
+            <div className="col-12 text-center"><h2>Front-End</h2><hr></hr></div> 
+            <div class="col-12  col-lg-6 my-3">
+            <div class="media">
+                    <img class="d-flex mr-3 img-thumbnail align-self-center small-image"
+                            src={tushar} alt="YASH PANDEY"></img>
+                    <div class="media-body">
+                        <h2 class="mt-2">YASH PANDEY
+                        </h2>
+                        <p class="d-none d-sm-block">Musician/ Coder/ Blogger/ Youtube/ Developer/ Influencer/ Football /TT / Dancer/ Playboy/ Mumbaikar</p>
+                        <br></br>
+                        <a className=" btn text-dark" href="https://"><i className="fab fa-linkedin fa-2x"></i></a>
+                        <a className=" btn text-dark" href="https://"><i className="fab fa-github fa-2x"></i></a>
+                        <a className=" btn text-dark" href="mailto:iiitranchi@contact.info"><i className="fas fa-envelope fa-2x"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6 my-3">
+            <div class="media">
+                    <img class="d-flex mr-3 img-thumbnail align-self-center small-image"
+                            src={tushar} alt="YASH PANDEY"></img>
+                    <div class="media-body">
+                        <h2 class="mt-2">YASH PANDEY
+                        </h2>
+                        <p class="d-none d-sm-block">Musician/ Coder/ Blogger/ Youtube/ Developer/ Influencer/ Football /TT / Dancer/ Playboy/ Mumbaikar</p>
+                        <br></br>
+                        <a className=" btn text-dark" href="https://"><i className="fab fa-linkedin fa-2x"></i></a>
+                        <a className=" btn text-dark" href="https://"><i className="fab fa-github fa-2x"></i></a>
+                        <a className=" btn text-dark" href="mailto:iiitranchi@contact.info"><i className="fas fa-envelope fa-2x"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row row-content align-items-center"> 
+            <div className="col-12 text-center"><h2>Back-end</h2><hr></hr></div> 
+            <div class="col-12  col-lg-6">
+            <div class="media">
+                    <img class="d-flex mr-3 img-thumbnail align-self-center small-image"
+                            src={tushar} alt="YASH PANDEY"></img>
+                    <div class="media-body">
+                        <h2 class="mt-2">YASH PANDEY
+                        </h2>
+                        <p class="d-none d-sm-block">Musician/ Coder/ Blogger/ Youtube/ Developer/ Influencer/ Football /TT / Dancer/ Playboy/ Mumbaikar</p>
+                        <br></br>
+                        <a className=" btn text-dark" href="https://"><i className="fab fa-linkedin fa-2x"></i></a>
+                        <a className=" btn text-dark" href="https://"><i className="fab fa-github fa-2x"></i></a>
+                        <a className=" btn text-dark" href="mailto:iiitranchi@contact.info"><i className="fas fa-envelope fa-2x"></i></a>
+                    </div>
+            </div>
+            </div>
+            <div class="col-12 col-lg-6">
+            <div class="media">
+                    <img class="d-flex mr-3 img-thumbnail align-self-center small-image"
+                            src={tushar} alt="YASH PANDEY"></img>
+                    <div class="media-body">
+                        <h2 class="mt-2">YASH PANDEY
+                        </h2>
+                        <p class="d-none d-sm-block">Musician/ Coder/ Blogger/ Youtube/ Developer/ Influencer/ Football /TT / Dancer/ Playboy/ Mumbaikar</p>
+                        <br></br>
+                        <a className=" btn text-dark" href="https://"><i className="fab fa-linkedin fa-2x"></i></a>
+                        <a className=" btn text-dark" href="https://"><i className="fab fa-github fa-2x"></i></a>
+                        <a className=" btn text-dark" href="mailto:iiitranchi@contact.info"><i className="fas fa-envelope fa-2x"></i></a>
+                    </div>
+            </div>
+            </div>
+        </div>
+        </div>
+      </div>
+      <div className="modal-footer">
+      <p className="text-dark col-12 text-center">Web Team IIITRanchi © Copyright 2020 </p>
+      </div>
+    </div>
+  </div>
+</div>
     <footer className="footer pb-0">
         <div className="container">
             <div className="row"> 
@@ -42,7 +162,7 @@ class Footer extends Component {
         <div className="container-fluid">
             <div className="row justify-content-center mt-5 mb-0 pt-3"> 
                 <div  className="col-2 col-sm-1 ">
-                    <a id="dev" className=" btn text-white " href="https://"><i className="fas fa-laptop-code fa-2x"></i><span>Developers</span></a>
+                    <a id="dev" className=" btn text-white " data-toggle="modal" data-target="#exampleModal"><i className="fas fa-laptop-code fa-2x"></i><span>Developers</span></a>
                  </div>   
                     
                  <div className="col-6 col-sm-10 text-center">
