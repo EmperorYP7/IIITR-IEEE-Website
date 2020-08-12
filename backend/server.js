16a9e542fc18d0d22e69665ecd543e661121bbf8
RishavMz <58086511+RishavMz@users.noreply.github.com>
2020-08-13 00:02:01 +0530

    Added multer for image/pdf uploading

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const fileUpload = require('express-fileupload');
+//const fileUpload = require('express-fileupload');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -11,14 +11,14 @@ const db = mongoose.connection;
 db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('Connected to MongoDB!'));
 
-app.use(fileUpload());
+//app.use(fileUpload());
 app.use(express.json());
 app.use(cors());
 
 //-------------API Routes--------------
 
-const eventRouter = require('./routes/EventRoute');
-app.use('/api/eventdata', eventRouter);
+//const eventRouter = require('./routes/EventRoute');
+//app.use('/api/eventdata', eventRouter);
 
 const memberRouter = require('./routes/MemberRoute');
 app.use('/api/memberdata', memberRouter);
@@ -27,8 +27,11 @@ app.use('/api/memberdata', memberRouter);
 
 //-----------------Upload Routes--------------------
 
-const imageRouter = require('./routes/data/ImageRoute');
+//const imageRouter = require('./routes/data/ImageRoute');
+//app.use('/upload/image', imageRouter);
+const imageRouter = require('./routes/imagerouter');
 app.use('/upload/image', imageRouter);
+
 
 //--------------------------------------------------
 app.listen(port, () => {
