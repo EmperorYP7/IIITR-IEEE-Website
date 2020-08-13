e950ead5c4d1727c3de6d83968cbf70ee7d62a15
Yash Pandey <62606998+EmperorYP7@users.noreply.github.com>
2020-08-14 01:41:42 +0530

    minor update

--- backend/routes/data/ImageRoute.js
+++ backend/routes/data/ImageRoute.js
@@ -51,11 +51,7 @@ const storage6 = multer.diskStorage({
 });
 
 const upload6 = multer({ storage: storage6 })
-<<<<<<< HEAD
-router.post('/gallery', upload6.array('image'), (req, res) => {
-=======
 router.post('/gallery', upload6.array('gallery'), (req, res) => {
->>>>>>> 76c30b28d04bae1faf665bc60ea13174afa7d1b2
   try {
     res.send(req.file);
   }catch(err) {
