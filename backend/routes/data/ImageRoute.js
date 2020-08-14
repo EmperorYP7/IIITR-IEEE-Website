a36aa27ec48b7e8b7c4ed1ae2eea8aef2d60f830
Yash Pandey <62606998+EmperorYP7@users.noreply.github.com>
2020-08-14 13:33:20 +0530

    merging master

--- backend/routes/data/ImageRoute.js
+++ backend/routes/data/ImageRoute.js
@@ -1,62 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
+const fileUpload = require('express-fileupload');
+router.use(fileUpload());
 
-const storage1 = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, './backend/uploads/images/members'),
-    filename: (req, file, cb) => cb(null , file.originalname)
-});
+// Upload Endpoint
+router.post('/upload', (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
+  }
 
-const upload1 = multer({ storage: storage1 })
-router.post('/member', upload1.single('member'), (req, res) => {
-    try {
-      res.send(req.file);
-    }catch(err) {
-      res.send(400);
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/frontend/public/uploads/${file.name}`, err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
     }
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
   });
-
-
-const storage2 = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, './backend/uploads/images/events'),
-    filename: (req, file, cb) => cb(null , file.originalname)
 });
-
-const upload2 = multer({ storage: storage2 })
-router.post('/event', upload2.single('event'), (req, res) => {
-    try {
-      res.send(req.file);
-    }catch(err) {
-      res.send(400);
-    }
-});
-
-const storage5 = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, './backend/uploads/images/home/carousal'),
-  filename: (req, file, cb) => cb(null, file.originalname)
-});
-
-const upload5 = multer({ storage: storage5 })
-router.post('/home/carousal', upload5.single('image'), (req, res) => {
-  try {
-    res.send(req.file);
-  }catch(err) {
-    res.send(400);
-  }
-});
-
-const storage6 = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, './backend/uploads/images/gallery'),
-  filename: (req, file, cb) => cb(null, file.originalname)
-});
-
-const upload6 = multer({ storage: storage6 })
-router.post('/gallery', upload6.array('image'), (req, res) => {
-  try {
-    res.send(req.file);
-  }catch(err) {
-    res.send(400);
-  }
-});
-
-module.exports = router;
