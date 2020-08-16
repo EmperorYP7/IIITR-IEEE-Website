1d4fcd1091cdd5a32073debb948754de047935cb
tusharjain0022 <tusharjain0022@gmail.com>
2020-08-17 00:53:31 +0530

    navbar bugs fixed and collapsed menu styled

--- frontend/src/components/Main/Navbar/Navbar.js
+++ frontend/src/components/Main/Navbar/Navbar.js
@@ -1,35 +1,37 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css';
+import IEEElogo from './ieee-logo.png';
 import { Link } from 'react-router-dom';
 function Navbar() {
     const [active, setActive] = useState('Home');
         return (
-            <nav className="navbar navbar-expand-sm fixed-right navbar-light bgrnd py-3"  id="scrollup">
+            <nav className="navbar navbar-expand-md fixed-right navbar-light bgrnd py-3"  id="scrollup">
+                 <Link to ="/" className="navbar-brand"><img src={IEEElogo} className="logo"></img></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon "></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto ">
+                    <ul className="navbar-nav ml-auto text-center">
                         <Link to ="/" id="navLink">
-                            <li className={(active==='Home')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} onClick={() => setActive('Home')} >Home</li>
+                        <hr className="d-md-none"></hr><li className={(active==='Home')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} onClick={() => setActive('Home')} >Home</li>
                         </Link>
 
                         <Link to="/events" id="navLink">
-                                <li className={(active==='Events')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} onClick={() => setActive('Events')}>Events</li>
+                        <hr className="d-md-none"></hr> <li className={(active==='Events')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} onClick={() => setActive('Events')}>Events</li>
                         </Link>
 
                         <Link to ="/resources" id="navLink">
-                            <li className={(active==='Resources')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} onClick={() => setActive('Resources')}>Resources</li>
+                        <hr className="d-md-none"></hr> <li className={(active==='Resources')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} onClick={() => setActive('Resources')}>Resources</li>
                         </Link>
 
                         <Link to ="/members" id="navLink">
-                            <li className={(active==='Members')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} onClick={() => setActive('Members')}>Members</li>
+                        <hr className="d-md-none"></hr> <li className={(active==='Members')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} onClick={() => setActive('Members')}>Members</li>
                         </Link>
 
                         <Link to ="/gallery" id="navLink">
-                            <li className={(active==='Gallery')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} onClick={() => setActive('Gallery')}>Gallery</li>
+                        <hr className="d-md-none"></hr> <li className={(active==='Gallery')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} onClick={() => setActive('Gallery')}>Gallery</li>
                         </Link>
                     </ul>
                 </div>
