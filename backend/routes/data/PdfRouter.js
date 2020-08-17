05a9926ab77ec8f2d739365a6d579fc10f37f9b2
RishavMz <58086511+RishavMz@users.noreply.github.com>
2020-08-17 16:07:14 +0530

    PDF OP

--- backend/routes/data/PdfRouter.js
+++ backend/routes/data/PdfRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const path = require('path');
+
 
 //------------------------Resource Upload--------------------------
 
