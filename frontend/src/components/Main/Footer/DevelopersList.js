e0e0e431b3c2e60b180ccf00ade48df6c92fe9a4
EmperorYP7 <pandeyyash04@gmail.com>
2020-08-18 17:41:29 +0530

    adding details to developer

--- frontend/src/components/Main/Footer/DevelopersList.js
+++ frontend/src/components/Main/Footer/DevelopersList.js
@@ -2,12 +2,12 @@ export const DEVELOPERS =
     [
         {
         id: 0,
-        name:'Name',
-        intro: 'intro',
-        img:'',
-        linkedin: 'https://',
-        github:'https://',
-        mail:'https://'
+        name:'Yash Pandey',
+        intro: 'The GitHub Guy. Opensource Developer.',
+        img:'assets/images/yash.jpeg',
+        linkedin: 'https://www.linkedin.com/in/yash-p-1793b185/',
+        github:'https://github.com/EmperorYP7',
+        mail:'mailto:yash.btech.cs19@iiitranchi.ac.in'
        },
        {
         id: 1,
