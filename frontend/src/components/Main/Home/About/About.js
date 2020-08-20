3fba5086273d690456ac16b392e7e8c0b3246ffe
EmperorYP7 <pandeyyash04@gmail.com>
2020-08-20 20:48:02 +0530

    responsive about

--- frontend/src/components/Main/Home/About/About.js
+++ frontend/src/components/Main/Home/About/About.js
@@ -33,51 +33,42 @@ return (
   <div className="col-12 text-center text-white">
   <h3>About Us</h3><hr></hr>
   </div>
-  <div className="col-12 col-md-4">
+  <div className="col-sm-12 col-md-6 col-lg-4">
   <Card className="px-3 py-3 mt-5 text-center bg">
     {/* <Card.Img variant="top" src={logo} /> */}
     <span className="text-center"><i className="fas fa-puzzle-piece fa-5x icon-lightblue"></i></span>
     <Card.Body className="body">
-      <Card.Title className="cart-title text-white bold"><span className="text-lg">WHAT</span> is IEEE ?</Card.Title>
-      <Card.Text >
-        Institute of Electrical and Electronics Engineers
-        Institute of Electrical and Electronics Engineers
-        Institute of Electrical and Electronics Engineers
-        Institute of Electrical and Electronics Engineers
-        </Card.Text>
+      <Card.Title className="cart-title text-white bold"><span className="text-lg">What</span> is IEEE?</Card.Title>
+      <Card.Text>
+        IEEE is the world's largest technical professional organization dedicated to advancing technology for the benefit of humanity.
+      </Card.Text>
     </Card.Body>
     {/* <Card.Footer className="bg-info">
     </Card.Footer> */}
   </Card>
   </div>
-  <div className="col-12 col-md-4">
+  <div className="col-sm-12 col-md-6 col-lg-4">
   <Card className="px-3 py-3 mt-5 text-center bg ">
     {/* <Card.Img variant="top" src={logo} /> */}
     <span className="text-center"><i className="fas fa-tasks fa-5x icon-lightblue"></i></span>
     <Card.Body className="body">
-      <Card.Title className="cart-title text-white bold "><span className="text-lg">WHY</span> is IEEE ?</Card.Title>
+      <Card.Title className="cart-title text-white bold "><span className="text-lg">Why</span> IEEE?</Card.Title>
       <Card.Text>
-      Institute of Electrical and Electronics Engineers
-        Institute of Electrical and Electronics Engineers
-        Institute of Electrical and Electronics Engineers
-        Institute of Electrical and Electronics Engineers
+        IEEE houses an unrivaled network of professionals, experts, and advisors that can help shape your career.
       </Card.Text>
     </Card.Body>
     {/* <Card.Footer>
     </Card.Footer> */}
   </Card>
   </div>
-  <div className="col-12 col-md-4">
+  <div className="col-sm-12 col-md-12 col-lg-4">
   <Card className="px-3 py-3 mt-5 text-center bg">
     {/* <Card.Img variant="top" src={logo} /> */}
     <span className="text-center"><i className="fas fa-university fa-5x icon-lightblue"></i></span>
     <Card.Body className="body">
       <Card.Title className="cart-title text-white bold"><span className="text-lg">BENEFITS</span> of IEEESB </Card.Title>
       <Card.Text>
-      Institute of Electrical and Electronics Engineers
-        Institute of Electrical and Electronics Engineers
-        Institute of Electrical and Electronics Engineers
-        Institute of Electrical and Electronics Engineers
+      IEEE membership delivers access to the industry's most essential technical information and provides networking opportunities both locally and globally.
       </Card.Text>
     </Card.Body>
     {/* <Card.Footer className="bg-info">
