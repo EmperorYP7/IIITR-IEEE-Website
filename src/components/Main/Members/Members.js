4699992224bc21ee037c883e8931dca693d398fb
tusharjain0022 <59157988+tusharjain0022@users.noreply.github.com>
2020-08-26 10:34:54 +0530

    Update Members.js

--- src/components/Main/Members/Members.js
+++ src/components/Main/Members/Members.js
@@ -66,7 +66,7 @@ class Members extends Component {
                 <div className="container">
                     <div className="row justify-content-center">
                         <div className="col-12 text-center member-header">
-                            <h2>Member</h2>
+                            <h2>Members</h2>
                             <hr></hr>
                         </div>
                         <div className="col-12">
