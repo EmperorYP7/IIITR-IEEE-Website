c45157e3b8f484176b93723cba57d1529c60b58b
Yash Pandey (YP) <yash.btech.cs19@iiitranchi.ac.in>
2021-02-25 02:37:27 +0530

    revamped express

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const port = 6000 || process.env.PORT;
+var createError = require('http-errors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
@@ -13,6 +26,10 @@ const db = mongoose.connection;
 db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('Connected to MongoDB!'));
 
+// --------------Index routes--------------
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 //-------------API Routes--------------
 
@@ -46,6 +63,20 @@ app.use('/upload/pdf', pdfRouter);
 
 //--------------------------------------------------
 
-app.listen(port, () => {
-    console.log(`Server started on port : ${port}`)
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
