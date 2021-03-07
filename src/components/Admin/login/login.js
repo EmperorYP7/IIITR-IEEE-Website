fea5c3004aa62231a029c04240890052614b8be6
Yash Pandey (YP) <yash.btech.cs19@iiitranchi.ac.in>
2021-03-07 14:26:47 +0530

    added admin
    Signed-off-by: Yash Pandey (YP) <yash.btech.cs19@iiitranchi.ac.in>

--- src/components/Admin/login/login.js
+++ src/components/Admin/login/login.js
@@ -1,113 +1,106 @@
-import React, { Component } from 'react';
-import './login.css';
-import ReactDOM from 'react-dom';
-import Events from '../Events/Events';
-import Members from '../Members/Members';
-import Notices from '../Notices/Notices';
-import Resources from '../Resources/Resources';
-import Gallery from '../Gallery/Gallery';
-import Conference from '../Resources/Conference';
+import React, { useState } from "react";
+import axios from "axios";
+import Input from "./input";
+import "./loginform.css";
+import Notify from '../../Notify';
+import { ToastContainer } from "react-toastify";
 
+function LoginForm(props) {
+  //object
+  const [account, setAccount] = useState({
+    username: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+	const [disabled, setStatus] = useState(false);
+  const validate = () => {
+    const err = {};
+    if (account.username.trim() === "") err.username = "Username is Required";
+    if (account.password.trim() === "") err.password = "Password is Required";
+    return Object.keys(err).length === 0 ? null : err;
+  };
 
-class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.changeHandler = this.changeHandler.bind(this);
-        this.login = this.login.bind(this);
-        this.data = this.data.bind(this);
-
-        this.state = {
-            user: "",
-            pass: "",
-            user1: "21232f297a57a5a743894a0e4a801fc3",
-            pass1 : "4f63e2a7118502d8eae77ecdd4eaf66f",
-            auth : 0
-        }
+  const validateProperty = (input) => {
+    if (input.name === "username") {
+      if (input.value.trim() === "") return "Username can't be empty.";
     }
-
-    login = (e) => {
-        e.preventDefault();
-        const md5   = require("blueimp-md5");
-        //console.log((this.state.pass1).localeCompare( (this.state.pass)));
-        //const parsedUser = this.md5(this.state.user);
-        // this.state.user = md5(this.state.user);
-        // this.state.pass = md5(this.state.pass);
-        if(((this.state.pass1).localeCompare(md5(this.state.pass)) === 0) && ((this.state.user1).localeCompare(md5(this.state.user)) === 0)){
-            this.setState({auth : 1});
-            ReactDOM.render(<div><h1 className="text-center">Admin Portal</h1><hr></hr></div>, document.getElementById('FillHere0'));
-            ReactDOM.render(<Events/>, document.getElementById('FillHere2'));
-            ReactDOM.render(<Notices/>, document.getElementById('FillHere3'));
-            ReactDOM.render(<Members/>, document.getElementById('FillHere4'));
-            ReactDOM.render(<Gallery/>, document.getElementById('FillHere5'));
-            ReactDOM.render(<Resources/>, document.getElementById('FillHere6'));
-            ReactDOM.render(<Conference/>, document.getElementById('FillHere7'));
-
-        }
-        else{
-            ReactDOM.render(<div className="alert alert-danger mt-3" role="alert">
-            Wrong username or Password !!
-          </div>, document.getElementById('alert'));
-        }
-
+    if (input.name === "password") {
+      if (input.value.trim() === "") return "Password can't be empty.";
     }
-    componentDidMount() {
+  };
 
-    }
-    data(){
-        if(this.state.auth === 1){
-            console.log("See");
-            return <h1>There is itThere is itThere is itThere is itThere is itThere is itThere is it</h1>;
-        }
-    }
+  const handleChange = ({ currentTarget: input }) => {
+    const err = { ...errors };
+    const errorMessage = validateProperty(input);
+    if (errorMessage) err[input.name] = errorMessage;
+    else err[input.name] = "";
 
-    changeHandler = (e) => {
-        this.setState({
-            ...this.state,
-            [e.target.name]: e.target.value
-        });
-    }
+    const acc = { ...account };
+    acc[input.name] = input.value;
+    setAccount({ ...account, ...acc });
+    setErrors({ ...errors, ...err });
+  };
 
-    render() {
-        return (
-        <div>
-            
-            <div id="FillHere0">
-            <div id="alert"></div>
-            <div className="container ">
-                <div className="row justify-content-md-center">
-                    <div className="col-12 col-lg-5 col-md-8 login">
-                    <div className="text-center h1">Admin Login</div><hr></hr>
-                <form className="align-content-center" onSubmit={this.login}>
-                    <div className="form-group">
-                        <label htmlFor="name">Username</label>
-                        <input type="text" required={true} className="form-control" name="user" onChange={this.changeHandler} placeholder="Enter username" value={this.state.user} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="designation">Password</label>
-                        <input type="password" className="form-control" name="pass" onChange={this.changeHandler} value={this.state.pass} placeholder="Enter password" />
-                    </div>
-                    <div className="text-center"><button type="submit" className="btn btn-primary login-button">Log In</button></div>
-                    
-                </form>
-                <p className="text-muted text-center mt-5 ">Web Team IIITRanchi © Copyright 2020
-              </p>
-                    </div>
-                    
-                </div>
-                
-            </div>
-            </div>
-            <div id="FillHere1"></div>
-            <div id="FillHere2"></div>
-            <div id="FillHere3"></div>
-            <div id="FillHere4"></div>
-            <div id="FillHere5"></div>
-            <div id="FillHere6"></div>
-            <div id="FillHere7"></div>
+  const handleSubmit = (e) => {
+		setStatus(true);
+    e.preventDefault();
+    const err = validate();
+    setErrors({ ...errors, ...err });
+    if (errors.password !== "") return;
+    const payload = {
+        username: `${account.username}`,
+        pass: `${account.password}`,
+    };
+    axios
+      .post(`${process.env.REACT_APP_API || ''}/admin/login`, payload)
+      .then((result) => {
+				window.localStorage.setItem("token", result.data.accessToken);
+				Notify("Login successful, Redirecting . . .", "success");
+				setTimeout(() => {
+					props.history.push("/admin");
+				}, 3000);
+      })
+      .catch((err) => {
+				setStatus(false);
+        Notify(err.response.data.message, "err");
+      });
+  };
 
-
-        </div>
-        );
-    }
+  return (
+    <div className="text-white pt-5 admin-login">
+      <h1 className="text-center">Admin Login</h1>
+      <br></br>
+      <div className="container adm-form pt-4 pd-4">
+        <form onSubmit={handleSubmit}>
+          <Input
+            errors={errors.username}
+            name="username"
+            label="Username"
+            value={account.username}
+            onChange={handleChange}
+            type="text"
+            readOnly={true}
+          />
+          <Input
+            errors={errors.password}
+            name="password"
+            label="Password"
+            value={account.password}
+            onChange={handleChange}
+            type="password"
+            readOnly={false}
+          />
+          <button
+            disabled={disabled}
+            className="btn btn-success submit-button"
+          >
+            <b>Login</b>
+          </button>
+        </form>
+      </div>
+      <ToastContainer autoClose={5000} />
+    </div>
+  );
 }
-export default Login;
+
+export default LoginForm;
